07a2e3f1070d9ba78cd96d81499c56fc31c5537f
simchacorcos <simcha8199@gmail.com>
2024-10-15 12:48:12 +0300

    book changes

--- ShWeb/Controllers/BookController.cs
+++ ShWeb/Controllers/BookController.cs
@@ -17,24 +17,10 @@ namespace ShWeb.Controllers
         }
 
         [HttpGet]
-        public List<Book> AllBooks()
-        {
-            return Cx.Books.ToList();
-        }
-
-        [HttpGet]
         public async Task<List<Book>> AllBooksIncludeSubjects()
         {
             return await Cx.Books.Include(b => b.Subjects).ToListAsync();
         }
-
-        //[HttpGet]
-        //public async Task<List<Book>> AllBooksIncludeSubjectsAndText()
-        //{
-        //    return await Cx.Books
-        //        .Include(b => b.Subjects)
-        //        .ThenInclude(s => s.SubjectText).ToListAsync();
-        //}
 
         [HttpGet("{SubjectID}")]
         public async Task<SubjectText> GetSubjectText(int SubjectID)
